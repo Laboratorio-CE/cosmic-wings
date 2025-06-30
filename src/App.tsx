8d0eb1f44eb92bcae8466c3bed87db6a62c827f2
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-30 18:01:43 -0300

    feat: Adicionado componente de instruções
    Adicionado componente de instruções

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Instructions from "./components/Instructions";
+
 function App() {
 
   return (
     <div className="bg-slate-800 text-cyan-50 h-screen flex flex-col items-center justify-center">
-      
+      <Instructions />
     </div>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class Menu extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+      <div>Menu</div>
+    )
+  }
+}
