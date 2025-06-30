b737227e16f050627b3e01794fc81ceb2e19c9f9
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-30 18:21:56 -0300

    feat: Adicionado background com efeito de estrelas
    Adicionado background com efeito de estrelas, componentes dos botões de ligar/desligar mídia.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Instructions from "./components/Instructions";
+import Background from './components/Background';
 
 function App() {
 
   return (
-    <div className="bg-slate-800 text-cyan-50 h-screen flex flex-col items-center justify-center">
+    <div className="cosmic-gradient text-cyan-50 h-screen flex flex-col items-center justify-center">
+      <Background starCount={5000} />
       <Instructions />
     </div>
   );
